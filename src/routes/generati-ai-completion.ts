cf05eea28a2f32792836dc1d8059b21395f7cbf0
diegohenriquecode <diegohenriquecode@gmail.com>
2023-09-14 13:14:31 -0300

    feat: finish release 1.0

--- src/routes/generati-ai-completion.ts
+++ src/routes/generati-ai-completion.ts
@@ -10,11 +10,11 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
     try {
       const bodySchema = z.object(({
         videoId: z.string().uuid(),
-        template: z.string(),
+        prompt: z.string(),
         temperature: z.number().min(0).max(1).default(0.5),
       }))
 
-      const { videoId, template, temperature } = bodySchema.parse(req.body);
+      const { videoId, prompt, temperature } = bodySchema.parse(req.body);
 
       const video = await prisma.video.findUniqueOrThrow({
         where: {
@@ -26,7 +26,7 @@ export async function generateAiCompletionRoute(app: FastifyInstance) {
         return reply.status(400).send({error: 'Video transcription was not generated yet.'})
       }
 
-      const promptMessage = template.replace('{transcription}', video.transcription)
+      const promptMessage = prompt.replace('{transcription}', video.transcription)
 
 
 
